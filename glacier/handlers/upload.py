df4cadee3f440c530f90dbcad4f27adabc622b1e
Huy Nguyen <36141077+huynguyent@users.noreply.github.com>
2019-05-12 23:03:19 +1000

    Implement multithreaded upload (#12)
    * Implement multithreaded upload using `multiprocessing.dummy`
    
    * Update `README.md` to mention multithreading

--- glacier/handlers/upload.py
+++ glacier/handlers/upload.py
@@ -1,6 +1,7 @@
 import os
 import boto3
 from botocore.utils import calculate_tree_hash
+from multiprocessing.dummy import Pool
 from glacier.printer import success, info
 from glacier.models.archive_part import ArchivePart
 
@@ -17,17 +18,15 @@ def handler(args):
 def upload_archive(client, vault_name, file_name, description, concurrency):
     file_size = os.stat(file_name).st_size
     part_size = calculate_part_size(file_size)
-    parts = generate_archive_parts(file_size, part_size)
 
     info('Initiating multipart upload')
     upload_id = client.initiate_multipart_upload(vaultName=vault_name, partSize=str(part_size),
                                                  archiveDescription=description)['uploadId']
 
-    for archive_part in parts:
-        info(f'Uploading part {archive_part.order + 1} (of {len(parts)})')
-        data = read_bytes(file_name, archive_part.start_byte, archive_part.part_size)
-        client.upload_multipart_part(vaultName=vault_name, uploadId=upload_id, range=archive_part.range, body=data)
-        info(f'Finished uploading part {archive_part.order + 1} (of {len(parts)})')
+    parts = generate_archive_parts(vault_name, file_name, upload_id, file_size, part_size)
+    info(f'Uploading files in {len(parts)} parts')
+    pool = Pool(concurrency)
+    pool.map(upload_archive_part, parts)
 
     info('Verifying checksum')
     checksum = calculate_tree_hash(open(file_name, 'rb'))
@@ -35,28 +34,38 @@ def upload_archive(client, vault_name, file_name, description, concurrency):
                                             checksum=checksum)
 
 
-def generate_archive_parts(file_size, part_size):
+def upload_archive_part(archive_part):
+    print(f'Uploading part {archive_part.order + 1}')
+    client = boto3.client('glacier')
+    data = read_bytes(archive_part.file_name, archive_part.start_byte, archive_part.part_size)
+    client.upload_multipart_part(vaultName=archive_part.vault_name, uploadId=archive_part.upload_id,
+                                 range=archive_part.range,
+                                 body=data)
+    print(f'Finished uploading part {archive_part.order + 1}')
+
+
+def generate_archive_parts(vault_name, file_name, upload_id, file_size, part_size):
     parts = []
     order = 0
     start_byte = 0
 
     while start_byte + part_size < file_size:
         range = f"bytes {start_byte}-{start_byte + part_size - 1}/*"
-        archive_part = ArchivePart(order, start_byte, part_size, range)
+        archive_part = ArchivePart(order, vault_name, file_name, upload_id, start_byte, part_size, range)
         order += 1
         start_byte = start_byte + part_size
         parts.append(archive_part)
 
     final_part_size = file_size - start_byte
     range = f"bytes {start_byte}-{start_byte + final_part_size - 1}/*"
-    archive_part = ArchivePart(order, start_byte, final_part_size, range)
+    archive_part = ArchivePart(order, vault_name, file_name, upload_id, start_byte, final_part_size, range)
     parts.append(archive_part)
 
     return parts
 
 
 def calculate_part_size(file_size):
-    part_size = 1024 ** 2   # 1 MB
+    part_size = 8 * (1024 ** 2)  # 8 MB
     while (file_size / part_size) > PART_COUNT_LIMIT:
         part_size *= 2
     return part_size
@@ -74,5 +83,5 @@ def parse_args(args):
     vault_name = args['<vault_name>']
     file_name = args['<file_name>']
     description = args['--description'] if args['--description'] else ''
-    concurrency = args['--concurrency']
+    concurrency = int(args['--concurrency'])
     return vault_name, file_name, description, concurrency
--- glacier/models/archive_part.py
+++ glacier/models/archive_part.py
@@ -1,12 +1,18 @@
 class ArchivePart:
-    def __init__(self, order, start_byte, part_size, range):
+    def __init__(self, order, vault_name, file_name, upload_id, start_byte, part_size, range):
         self.order = order
+        self.vault_name = vault_name
+        self.file_name = file_name
+        self.upload_id = upload_id
         self.start_byte = start_byte
         self.part_size = part_size
         self.range = range
 
     def __str__(self):
         return (f"order: {self.order}\n"
+                f"vault_name: {self.vault_name}\n"
+                f"file_name: {self.file_name}\n"
+                f"upload_id: {self.upload_id}\n"
                 f"start_byte: {self.start_byte}\n"
                 f"part_size: {self.part_size}\n"
                 f"range: {self.range}")
