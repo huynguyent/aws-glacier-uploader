75c51cf5256fbeea0ba7630a8913dc3af0894454
Huy Nguyen <36141077+huynguyent@users.noreply.github.com>
2019-05-16 19:54:18 +1000

    Cleanup code in `create` module (#17)

--- glacier/handlers/create.py
+++ glacier/handlers/create.py
@@ -4,17 +4,8 @@ from glacier.printer import success
 
 
 def handler(args):
-    vault_name = args['<vault_name>']
-    account_id = '-'
     client = boto3.client('glacier')
+    vault_name = args['<vault_name>']
 
-    response = create_vault(client, vault_name, account_id)
+    response = client.create_vault(vaultName=vault_name)
     success(f"A new vault has been created at: {response['location']}")
-
-
-def create_vault(client, vault_name, account_id):
-    response = client.create_vault(
-        accountId=account_id,
-        vaultName=vault_name,
-    )
-    return response
